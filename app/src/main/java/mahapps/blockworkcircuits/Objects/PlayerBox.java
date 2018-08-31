cf1f7e84c54d807bc426f0378a4a41e269e9c614
Alex Mah <mahalex88@gmail.com>
2018-08-30 19:35:56 -0600

    Created a bit more into player box. Trying to commit to Object branch

--- app/src/main/java/mahapps/blockworkcircuits/Objects/PlayerBox.java
+++ app/src/main/java/mahapps/blockworkcircuits/Objects/PlayerBox.java
@@ -1,4 +1,9 @@
 package mahapps.blockworkcircuits.Objects;
 
-public class PlayerBox {
+public class PlayerBox extends BoxBase{
+
+    public PlayerBox(int xPos, int yPos){
+        super(xPos, yPos, new Colour(0, 0, 0));
+    }
+
 }
--- app/src/main/java/mahapps/blockworkcircuits/TestZone.java
+++ app/src/main/java/mahapps/blockworkcircuits/TestZone.java
@@ -6,20 +6,23 @@ import android.graphics.Color;
 
 import mahapps.blockworkcircuits.Objects.*;
 
-//New Comment
+
 
 public class TestZone {
 
     private BoxBase box;
+    private PlayerBox playerBox;
 
     TestZone(){
 
         box = new BoxBase(90, 90, new Colour(100, 100, 100));
+        playerBox = new PlayerBox(Constants.SCREEN_WIDTH/2, Constants.SCREEN_HEIGHT/2);
 
     }
 
     public void draw(Canvas canvas){
         canvas.drawColor(Color.WHITE);
         box.display(canvas);
+        playerBox.display(canvas);
     }
 }
